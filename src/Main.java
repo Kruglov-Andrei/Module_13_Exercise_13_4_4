b6f125833918d5c710db89107c1c86219aad4cb0
Kruglov Andrei <dus.sneg@gmail.com>
2022-01-20 16:53:52 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException{
+        ConsoleClock cc = new ConsoleClock();
+        cc.start();
+        Thread.sleep(3000);
+        cc.interrupt();
+    }
+}
